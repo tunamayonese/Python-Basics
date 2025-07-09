298416e37dc96f7f439f58c88f51c28f1d50e894
Kyan Hartadi <kyanra.hartadi@gmail.com>
2025-07-09 15:32:29 +0700

    variables

---
+++ variables.py
@@ -0,0 +1,33 @@
+#piece of space wher we cna store information
+
+age = 12
+name = "fred"
+temp = 48.0
+is_ok = True
+uh_oh = None #none
+
+print(type(age))
+print(type(name))
+print(type(temp))
+print(type(is_ok))
+print(type(uh_oh))
+
+# casting: changing variable type
+age = "12"
+int(age)
+
+#quite common statements kind of nested with each other
+print(type(float(age)))
+
+#can declare multiple variables at te same time
+x, y, z = 12, "drum", 56.5
+print(x, y, z)
+
+#string variable
+my_name = "fred"
+my_age = 45
+
+myfs = f"{}" # curly braces placeholder for variable 
+# / scaping
+
+print(my_name, "no", my_age, "ssd")
