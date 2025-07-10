7030686adf978f0b2674d29cc33cda0472516e42
Kyan Hartadi <kyanra.hartadi@gmail.com>
2025-07-10 23:42:46 +0700

    exploring the diff types of operators

---
+++ operators.py
@@ -0,0 +1,38 @@
+x, y, z = 18, 12.0, "22"
+
+# + - * / %
+
+ans = x + 3
+print(type(ans))
+
+ans = x - 3
+print(type(ans))
+
+ans = x * 3
+print(type(ans))
+
+ans = x / 4
+print(type(ans)) #unique
+
+ans = x + y
+print(ans, type(ans))
+
+word = "hello"
+ans = word + z
+print(ans)
+
+ans = word * x
+print(ans)
+
+#increment nd decrement
+
+x += 3
+print(x)
+
+x-= 3
+print(x)
+
+x *= 3
+print(x)
+
+#short hand assignments
