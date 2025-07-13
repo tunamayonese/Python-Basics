f0d5dad3570bd877606957fef727c81ba485e78f
Kyan Hartadi <kyanra.hartadi@gmail.com>
2025-07-13 23:14:12 +0700

    getting elements

--- lists.py
+++ lists.py
@@ -1,7 +1,8 @@
 
 # list example
-list = [12, "hello, 24, True, False, True, 16.0]
-
+list = [12, "hello", 24, True, False, True, 16.0]
+# index [0]   [1]    [2]  [3]   [4]    [5]   [6]
+        
 print(list)
 
 # length of said list
@@ -9,3 +10,6 @@ ans = lens(list)
 
 print(list)
 
+# getting an element
+ans = list[1]
+print (ans)
